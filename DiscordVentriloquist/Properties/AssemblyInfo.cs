7adb60cbae2ef3f95f9a94e2e5bc3c6559471cda
YotaXP <yotaxp+git@gmail.com>
2018-08-01 23:49:23 -0400

    Signed

--- DiscordVentriloquist/Properties/AssemblyInfo.cs
+++ DiscordVentriloquist/Properties/AssemblyInfo.cs
@@ -7,10 +7,10 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DiscordVentriloquist")]
+[assembly: AssemblyTitle("Discord Ventriloquist")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("YotaXP")]
 [assembly: AssemblyProduct("DiscordVentriloquist")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
